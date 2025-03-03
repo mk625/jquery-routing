b25b1d818c316a473cfd6c225dc32613a7f5ea9a
Muthu Kumar <88616865+mk625@users.noreply.github.com>
2025-03-03 18:42:25 +0530

    code update

--- js/route.js
+++ js/route.js
@@ -1,31 +1,30 @@
-$j(document).ready(function () {
-    function loadPage(page_container, hash) {
-        if (!hash || hash === "#/" || hash === "#") {
-            hash = "home"; // Default to home
-        } else {
-            hash = hash.replace("#/", ""); // Remove #/
-        }
-
-        append_to_container(page_container, hash);
+function loadPage(page_container, hash) {
+    if (!hash || hash === "#/" || hash === "#") {
+        hash = "home"; // Default to home
+    } else {
+        hash = hash.replace("#/", ""); // Remove #/
     }
 
-    function navigate(page_container, hash) {
-        window.location.hash = "#/" + hash;
-        loadPage(page_container, hash);
-    }
+    append_to_container(page_container, hash);
+}
 
-    $j(window).on("hashchange", function () {
-        var hash = window.location.hash;
-        loadPage("#dynamic-page-holder", hash);
-    });
+function navigate(page_container, hash) {
+    window.location.hash = "#/" + hash;
+    loadPage(page_container, hash);
+}
 
-    $j(".nav-btn").on("click", function (e) {
-        e.preventDefault();
-        var page_container = $j(this).attr("data-page-cont");
-        var hash = $j(this).attr("data-page").replace("/", "");
-        navigate(page_container, hash);
-    });
-
-    // Ensure correct page loads on refresh
-    loadPage("#dynamic-page-holder", window.location.hash);
+$j(window).on("hashchange", function () {
+    var hash = window.location.hash;
+    loadPage("#dynamic-page-holder", hash);
 });
+
+$j(".nav-btn").on("click", function (e) {
+    e.preventDefault();
+    console.log("nav btn clicked")
+    var page_container = $j(this).attr("data-page-cont");
+    var hash = $j(this).attr("data-page").replace("/", "");
+    navigate(page_container, hash);
+});
+
+// Ensure correct page loads on refresh
+loadPage("#dynamic-page-holder", window.location.hash);
